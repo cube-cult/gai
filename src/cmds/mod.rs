dca28b0c3483a874d780caf9633bcf208ca866c7
nuttyCream <issanutty@gmail.com>
2025-12-10 07:40:39 -0800

    refactor(tui): Refactor TUI command to use global arguments

--- src/cmds/mod.rs
+++ src/cmds/mod.rs
@@ -10,7 +10,7 @@ pub fn run(args: &Cli) -> anyhow::Result<()> {
     match &args.command {
         Commands::Auth(a) => auth::run(&a.auth)?,
         Commands::Status(a) => status::run(a, &args.global)?,
-        Commands::TUI(a) => tui::run(a)?,
+        Commands::TUI(a) => tui::run(a, &args.global)?,
         Commands::Commit(a) => commit::run(a, &args.global)?,
         Commands::Log(a) => log::run(a, &args.global)?,
         Commands::Rebase => {}
--- src/cmds/tui.rs
+++ src/cmds/tui.rs
@@ -1,5 +1,16 @@
-use crate::args::TUIArgs;
+use crate::{
+    args::{GlobalArgs, TUIArgs},
+    state::State,
+    terminal::app::run_tui,
+};
 
-pub fn run(args: &TUIArgs) -> anyhow::Result<()> {
+pub fn run(
+    _args: &TUIArgs,
+    global: &GlobalArgs,
+) -> anyhow::Result<()> {
+    let state = State::new(global.config.as_deref())?;
+
+    run_tui(state.config, state.gai)?;
+
     Ok(())
 }
