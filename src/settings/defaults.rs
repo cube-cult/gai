88cf62951484b9308111a36dca698a4011c44a4a
nuttyCream <issanutty@gmail.com>
2025-12-11 04:15:02 -0800

    feat: implement new settings structure

--- src/settings/defaults.rs
+++ src/settings/defaults.rs
@@ -1,48 +1,62 @@
-use crate::providers::provider::{ProviderConfigs, ProviderKind};
+use super::{
+    CommitSettings, ContextSettings, PromptRules, PromptSettings,
+    TuiSettings,
+};
 
-use super::{AiConfig, CommitConfig, RuleConfig};
-
-impl Default for AiConfig {
+impl Default for PromptSettings {
     fn default() -> Self {
         Self {
-            provider: ProviderKind::Gai,
             system_prompt: None,
             commit_convention: None,
-            include_convention: true,
-            include_file_tree: true,
-            include_git_status: true,
-            include_untracked: true,
-            files_to_truncate: vec![],
-            rules: RuleConfig::default(),
-            provider_configs: ProviderConfigs::default(),
             hint: None,
+            extra: None,
         }
     }
 }
 
-impl Default for RuleConfig {
+impl Default for PromptRules {
     fn default() -> Self {
         Self {
             group_related_files: true,
-            no_file_splitting: true,
             separate_by_purpose: true,
             verbose_descriptions: true,
-            exclude_extension_in_scope: true,
+            extension_in_scope: false,
             allow_empty_scope: true,
             max_header_length: 52,
-            allow_body: true,
+            allow_body: false,
             max_body_length: 72,
         }
     }
 }
-
-impl Default for CommitConfig {
+impl Default for ContextSettings {
     fn default() -> Self {
         Self {
+            include_convention: false,
+            include_file_tree: false,
+            include_git_status: true,
+            include_untracked: true,
+            files_to_truncate: None,
+            include_log: true,
+            log_amount: 5,
+        }
+    }
+}
+
+impl Default for CommitSettings {
+    fn default() -> Self {
+        Self {
+            only_staged: false,
+            stage_hunks: false,
             capitalize_prefix: false,
             include_scope: true,
             include_breaking: true,
-            breaking_symbol: None,
+            breaking_symbol: '!',
         }
+    }
+}
+
+impl Default for TuiSettings {
+    fn default() -> Self {
+        Self {}
     }
 }
