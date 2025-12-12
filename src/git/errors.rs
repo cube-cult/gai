167ad1cbcad5e886f94cfdef160e363536ed5f24
nuttyCream <issanutty@gmail.com>
2025-12-12 04:29:15 -0800

    chore: initial error types

--- src/git/errors.rs
+++ src/git/errors.rs
@@ -1,4 +1,34 @@
+use std::error::Error;
+
 /// errors for git2rs
 /// or wrapper related
 /// error types
-pub enum GitError {}
+#[derive(Debug)]
+pub enum GitError {
+    Git2(git2::Error),
+}
+
+impl std::fmt::Display for GitError {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        match self {
+            GitError::Git2(error) => write!(f, "{}", error),
+        }
+    }
+}
+
+impl Error for GitError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            GitError::Git2(error) => Some(error),
+        }
+    }
+}
+
+impl From<git2::Error> for GitError {
+    fn from(e: git2::Error) -> Self {
+        Self::Git2(e)
+    }
+}
