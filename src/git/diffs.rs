d12a1d81737529b2f732959d442039481000fbbb
nuttyCream <issanutty@gmail.com>
2025-12-14 20:50:19 -0800

    chore: initial diff structs

--- src/git/diffs.rs
+++ src/git/diffs.rs
@@ -1,6 +1,3 @@
-use git2::{DiffDelta, DiffHunk, DiffLine, DiffOptions};
-use walkdir::WalkDir;
-
 use super::{
     repo::{GaiGit, GitRepo},
     settings::DiffStrategy,
@@ -27,33 +24,29 @@ pub enum DiffDeltaStatus {
     Renamed,
 }
 
+#[derive(Debug, Clone)]
+pub struct LineDiff {
+    pub kind: LineKind,
+    pub content: String,
+}
+
 pub struct Hunk {
     pub index: usize,
+    pub header: HunkHeader,
     /// raw content of
     /// what the hunk contains
     pub raw: String,
+    pub lines: Vec<LineDiff>,
+}
 
+pub struct HunkHeader {
     // copied from DiffHunk
     old_start: u32,
     old_lines: u32,
     new_start: u32,
     new_lines: u32,
-    // in bytes
-    header: u8,
-}
-
-impl Hunk {
-    pub fn from_diff_hunk(hunk: &DiffHunk) -> Self {
-        Self {
-            index: todo!(),
-            raw: todo!(),
-            old_start: todo!(),
-            old_lines: todo!(),
-            new_start: todo!(),
-            new_lines: todo!(),
-            header: todo!(),
-        }
-    }
+    // full raw header
+    raw: String,
 }
 
 /// taken from diffline::origin
@@ -84,12 +77,29 @@ impl LineKind {
     }
 }
 
+impl Hunk {
+    ///helper funcs for future ui/tui
+    pub fn additions(&self) -> Vec<&LineDiff> {
+        self.lines
+            .iter()
+            .filter(|l| l.kind == LineKind::Additions)
+            .collect()
+    }
+
+    pub fn deletions(&self) -> Vec<&LineDiff> {
+        self.lines
+            .iter()
+            .filter(|l| l.kind == LineKind::Additions)
+            .collect()
+    }
+}
+
 impl Diffs {
     pub fn create(
         repo: &GitRepo,
         strategy: &DiffStrategy,
     ) -> anyhow::Result<Self> {
-        let mut opts = DiffOptions::new();
+        let mut opts = git2::DiffOptions::new();
         opts.include_untracked(true)
             .recurse_untracked_dirs(true)
             .enable_fast_untracked_dirs(true);
@@ -130,7 +140,7 @@ impl Diffs {
 
 // file callback for diff delta loop
 fn file_cb(
-    diff_delta: DiffDelta,
+    diff_delta: git2::DiffDelta,
     strategy: &DiffStrategy,
 ) -> Option<FileDiff> {
     let path = diff_delta
@@ -159,7 +169,10 @@ fn file_cb(
     })
 }
 
-fn binary_cb(_delta: DiffDelta, _strategy: &DiffStrategy) -> bool {
+fn binary_cb(
+    _delta: git2::DiffDelta,
+    _strategy: &DiffStrategy,
+) -> bool {
     true
 }
 
@@ -180,12 +193,6 @@ pub struct HunkDiff {
     pub line_diffs: Vec<LineDiff>,
 }
 
-#[derive(Debug, Clone)]
-pub struct LineDiff {
-    pub diff_type: DiffType,
-    pub content: String,
-}
-
 /// taken from diffline::origin
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum DiffType {
@@ -199,148 +206,6 @@ impl GaiGit {
         &mut self,
         truncate_files: Option<&[String]>,
     ) -> Result<(), git2::Error> {
-        // start this puppy up
-        let mut opts = DiffOptions::new();
-        opts.include_untracked(true)
-            .recurse_untracked_dirs(true)
-            .enable_fast_untracked_dirs(true);
-
-        let repo = &self.repo;
-
-        let head = repo.head()?.peel_to_tree()?;
-        let diff = if self.only_staged {
-            repo.diff_tree_to_index(
-                Some(&head),
-                None,
-                Some(&mut opts),
-            )?
-        } else {
-            repo.diff_tree_to_workdir(Some(&head), Some(&mut opts))?
-        };
-
-        let mut gai_files: Vec<GaiFile> = Vec::new();
-
-        // hilarious
-        let files_to_truncate = if let Some(f) = truncate_files {
-            f
-        } else {
-            &Vec::new()
-        };
-
-        diff.print(git2::DiffFormat::Patch, |delta, hunk, line| {
-            let path = delta
-                .new_file()
-                .path()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_owned();
-
-            let should_truncate =
-                files_to_truncate.iter().any(|f| path.ends_with(f));
-
-            let gai_file =
-                match gai_files.iter_mut().find(|g| g.path == path) {
-                    Some(existing) => existing,
-                    None => {
-                        gai_files.push(GaiFile {
-                            path: path.clone(),
-                            should_truncate,
-                            hunks: Vec::new(),
-                        });
-                        gai_files.last_mut().unwrap()
-                    }
-                };
-
-            process_file_diff(&mut gai_file.hunks, &hunk, &line);
-
-            true
-        })?;
-
-        if self.only_staged {
-            return Ok(());
-        }
-
-        self.files = gai_files;
-
-        // handle untracked files here
-        for path in &self.status.u_new {
-            let should_truncate =
-                files_to_truncate.iter().any(|f| path.ends_with(f));
-
-            for entry in WalkDir::new(path)
-                .follow_links(true)
-                .into_iter()
-                .filter_map(|e| e.ok())
-            {
-                if entry.path().is_file()
-                    && let Ok(content) =
-                        std::fs::read_to_string(entry.path())
-                {
-                    let path = entry.path().to_str().unwrap();
-                    let lines: Vec<LineDiff> = content
-                        .lines()
-                        .map(|line| LineDiff {
-                            diff_type: DiffType::Additions,
-                            content: format!("{}\n", line),
-                        })
-                        .collect();
-
-                    self.files.push(GaiFile {
-                        path: path.to_owned(),
-                        should_truncate,
-                        hunks: vec![HunkDiff {
-                            header: format!(
-                                "New File {}",
-                                lines.len()
-                            ),
-                            line_diffs: lines,
-                        }],
-                    });
-                }
-            }
-        }
-
-        self.files.sort_by_key(|g| g.should_truncate);
-
         Ok(())
-    }
-}
-
-fn process_file_diff(
-    diff_hunks: &mut Vec<HunkDiff>,
-    hunk: &Option<DiffHunk>,
-    line: &DiffLine,
-) {
-    if let Some(h) = hunk {
-        let header = str::from_utf8(h.header())
-            .unwrap_or("not a valid utf8 header from hunk")
-            .to_owned();
-        let content = str::from_utf8(line.content())
-            .unwrap_or("not a valid utf8 line from hunk")
-            .to_owned();
-
-        let diff_type = match line.origin() {
-            '+' => DiffType::Additions,
-            '-' => DiffType::Deletions,
-            ' ' => DiffType::Unchanged,
-            _ => return,
-        };
-
-        let diff_line = LineDiff { diff_type, content };
-
-        // instead of storing the different types.
-        // we can just push line diffs in a clear order
-        // if i want to filter it out, i can do that
-        // later, this should just care about the diff itself
-        match diff_hunks.iter_mut().find(|h| h.header == header) {
-            Some(existing) => existing.line_diffs.push(diff_line),
-            None => {
-                diff_hunks.push(HunkDiff {
-                    header,
-                    line_diffs: vec![diff_line],
-                });
-            }
-        }
     }
 }
