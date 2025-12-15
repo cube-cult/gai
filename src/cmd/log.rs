f9ca578eac4b00d1f1aa96ca61ff1aed235ea108
nuttyCream <issanutty@gmail.com>
2025-12-15 04:31:45 -0800

    chore: disable deprecated code

--- src/cmd/log.rs
+++ src/cmd/log.rs
@@ -10,7 +10,7 @@ pub fn run(
 ) -> anyhow::Result<()> {
     let state = State::new(None)?;
 
-    let logs = state.gai.get_logs(args.number, args.reverse)?;
+    //let logs = state.gai.get_logs(args.number, args.reverse)?;
 
     //pretty_print_logs(&logs, global.compact)?;
 
