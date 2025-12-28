e567d83fd57a7d63ecd74ab66426f8270dbbb67b
nuttyCream <issanutty@gmail.com>
2025-12-27 17:34:11 -0800

    chore: tree impl, might delete later

--- src/print/tree.rs
+++ src/print/tree.rs
@@ -1,4 +1,8 @@
-use ratatui::{style::Style, widgets::Widget};
+use ratatui::{
+    style::Style,
+    text::Text,
+    widgets::{StatefulWidget, Widget},
+};
 
 // this is a util widget that helps
 // pretty printing trees
@@ -14,27 +18,229 @@ use ratatui::{style::Style, widgets::Widget};
 // the root be exposed as a "list" that
 // we can select through
 
-pub struct TreeItem {}
+pub struct TreeItem<'text> {
+    text: Text<'text>,
+    children: Vec<Self>,
+}
 
-pub struct Tree {
-    items: Vec<TreeItem>,
+pub struct Tree<'a> {
+    items: &'a [TreeItem<'a>],
 
     style: Style,
 
+    highlight_style: Style,
+
     /// pre - pipe "│"
-    other_child: String,
+    other_child: &'a str,
 
     /// connector - tee "├──"
-    other_entry: String,
+    other_entry: &'a str,
 
-    /// pre - no more siblings
-    final_child: String,
+    /// pre - no more siblings " "
+    final_child: &'a str,
 
-    /// connector - elbow
-    final_entry: String,
+    /// connector - elbow "└── "
+    final_entry: &'a str,
 }
 
-impl Widget for Tree {
+#[derive(Default, Clone)]
+pub struct TreeState {
+    /// optional selection
+    /// this should select over
+    /// the root tree nodes
+    /// and not go any deeper
+    /// i dont think ill support that
+    /// for now
+    pub selected: Option<usize>,
+
+    /// collapse trees
+    /// onto a single line
+    pub collapsed: bool,
+}
+
+impl TreeState {
+    pub fn flatten<'a>(
+        &self,
+        items: &'a [TreeItem<'a>],
+    ) -> Vec<Flattened<'a>> {
+        todo!()
+    }
+
+    pub fn select_next(&mut self) {
+        self.selected =
+            Some(self.selected.map_or(0, |i| i.saturating_add(1)));
+    }
+
+    pub fn select_prev(&mut self) {
+        self.selected =
+            Some(self.selected.map_or(0, |i| i.saturating_sub(1)));
+    }
+
+    pub fn selected(&self) -> Option<usize> {
+        self.selected
+    }
+}
+
+/// from tui-rs-tree-widget
+/// modified wihtout the Identifier
+/// and since we dont have any of the identifiers
+/// gonna need to know depth,
+/// likely stored here,
+/// as well as which root this belongs too
+pub struct Flattened<'text> {
+    pub item: &'text TreeItem<'text>,
+
+    pub depth: usize,
+    pub root: usize,
+
+    /// is this last in the tree?
+    pub is_last: bool,
+}
+
+impl<'text> TreeItem<'text> {
+    /// create new treeitem, needs children
+    pub fn new<T>(
+        text: T,
+        children: Vec<Self>,
+    ) -> Self
+    where
+        T: Into<Text<'text>>,
+    {
+        Self {
+            text: text.into(),
+            children,
+        }
+    }
+
+    /// create new leaf, no children
+    pub fn new_leaf<T>(text: T) -> Self
+    where
+        T: Into<Text<'text>>,
+    {
+        Self {
+            text: text.into(),
+            children: Vec::new(),
+        }
+    }
+
+    pub fn children(&self) -> &[Self] {
+        &self.children
+    }
+
+    /// add a child to treeitem
+    pub fn add_child(
+        &mut self,
+        child: Self,
+    ) {
+        self.children.push(child);
+    }
+
+    /// Get a reference to a child by index.
+    pub fn child(
+        &self,
+        index: usize,
+    ) -> Option<&Self> {
+        self.children.get(index)
+    }
+
+    /// Get a mutable reference to a child by index.
+    pub fn child_mut(
+        &mut self,
+        index: usize,
+    ) -> Option<&mut Self> {
+        self.children.get_mut(index)
+    }
+
+    /// text widget height
+    pub fn height(&self) -> usize {
+        self.text.height()
+    }
+}
+
+impl<'a> Tree<'a> {
+    pub fn new(items: &'a [TreeItem]) -> Self {
+        Self {
+            items,
+            style: Style::default(),
+            highlight_style: Style::default(),
+            other_child: "│ ",
+            other_entry: "├──",
+            final_child: "  ",
+            final_entry: "└──",
+        }
+    }
+
+    pub fn items(
+        mut self,
+        items: &'a [TreeItem],
+    ) -> Self {
+        self.items = items;
+        self
+    }
+
+    pub fn style(
+        mut self,
+        style: Style,
+    ) -> Self {
+        self.style = style;
+        self
+    }
+
+    pub fn highlight_style(
+        mut self,
+        style: Style,
+    ) -> Self {
+        self.highlight_style = style;
+        self
+    }
+
+    pub fn other_child(
+        mut self,
+        other_child: &'a str,
+    ) -> Self {
+        self.other_child = other_child;
+        self
+    }
+
+    pub fn other_entry(
+        mut self,
+        other_entry: &'a str,
+    ) -> Self {
+        self.other_entry = other_entry;
+        self
+    }
+
+    pub fn final_child(
+        mut self,
+        final_child: &'a str,
+    ) -> Self {
+        self.final_child = final_child;
+        self
+    }
+
+    pub fn final_entry(
+        mut self,
+        final_entry: &'a str,
+    ) -> Self {
+        self.final_entry = final_entry;
+        self
+    }
+}
+
+impl StatefulWidget for Tree<'_> {
+    type State = TreeState;
+
+    fn render(
+        self,
+        area: ratatui::prelude::Rect,
+        buf: &mut ratatui::prelude::Buffer,
+        state: &mut Self::State,
+    ) {
+        todo!()
+    }
+}
+
+impl Widget for Tree<'_> {
     fn render(
         self,
         area: ratatui::prelude::Rect,
@@ -42,6 +248,7 @@ impl Widget for Tree {
     ) where
         Self: Sized,
     {
-        todo!()
+        let mut state = TreeState::default();
+        StatefulWidget::render(self, area, buf, &mut state);
     }
 }
